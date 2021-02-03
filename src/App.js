8d1e6ebaab22e9dceb023fbf3031dfa96557c6c6
maxgen6 <62709031+maxgen6@users.noreply.github.com>
2021-02-03 15:43:51 +0300

    Developer (#2)
    * Done first task
    
    * Rewrite project, add routing and fixed bugs
    
    Co-authored-by: Valery Filipyev <valeranzudhdin1998@mail.ru>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
-function App() {
+import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer'
+
+import Home from './Components/Pages/Home/Home'
+import Create from './Components/Pages/Create/Create'
+import Tasks from './Components/Pages/Tasks/Tasks'
+import TaskDetail from './Components/Pages/Tasks/TaskDetail/TaskDetail'
+import TaskEdit from './Components/Pages/Tasks/TaskEdit/TaskEdit'
+
+
+
+const App = () => {
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+
+      <div className="content">
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/create" component={Create} />
+            <Route path="/tasks" component={Tasks} />
+            <Route path="/task/:id/edit" component={TaskEdit} />
+            <Route path="/task/:id" component={TaskDetail} />
+
+
+            <Redirect to="/" />
+          </Switch>
+        </div>
+      </div>
+
+      <Footer />
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
